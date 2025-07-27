ee5955d0fe2fd03b2519885e26dcecaae29812b6
sriramragav <srirag@gmail.com>
2025-07-27 19:25:37 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Index from "./pages/About";
+import About from "./pages/About";
 import Internships from "./pages/Internships";
 import Projects from "./pages/Projects";
 import Awards from "./pages/Awards";
