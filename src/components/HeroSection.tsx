fafa79f40ed3567eff1b26221558a27318f6c6fc
sriramragav <srirag@gmail.com>
2025-07-28 20:19:16 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -38,9 +38,6 @@ const HeroSection = () => {
                 Download Resume
               </Button>
             </a>
-            <Button variant="outline" size="lg" onClick={scrollToAbout}>
-              Learn More
-            </Button>
           </div>
 
           {/* Status */}
@@ -54,11 +51,6 @@ const HeroSection = () => {
               <span>Leader in Training</span>
             </div>
           </div>
-        </div>
-
-        {/* Scroll Indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-          <ArrowDown className="w-6 h-6 text-muted-foreground" />
         </div>
       </div>
     </section>
