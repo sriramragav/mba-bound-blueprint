8148095d808ecd80ae0dacf0b2d5b19b54ab6777
sriramragav <srirag@gmail.com>
2025-08-01 23:46:48 +0530

    Update AwardsSection.tsx

--- src/components/AwardsSection.tsx
+++ src/components/AwardsSection.tsx
@@ -3,10 +3,10 @@ import { Trophy, Award, Star, Medal, Rocket, School, Lightbulb } from 'lucide-re
 import { useState } from 'react';
 
 const schoolCertificates = [
-  'https://raw.githubusercontent.com/sriramragav/mba-bound-blueprint/main/cert1.jpeg',
-  'https://raw.githubusercontent.com/sriramragav/mba-bound-blueprint/main/cert2.jpeg',
-  'https://raw.githubusercontent.com/sriramragav/mba-bound-blueprint/main/cert1.jpeg',
-  'https://raw.githubusercontent.com/sriramragav/mba-bound-blueprint/main/cert2.jpeg'
+  '/cert1.jpeg',
+  '/cert2.jpeg',
+  '/cert1.jpeg',
+  '/cert2.jpeg'
 ];
 
 const AwardsSection = () => {
