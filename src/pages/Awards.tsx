38187b26b419eed2ab60ab6d9ba3efb2b3c0df56
sriramragav <srirag@gmail.com>
2025-08-01 12:52:50 +0530

    Update Awards.tsx

--- src/pages/Awards.tsx
+++ src/pages/Awards.tsx
@@ -16,7 +16,19 @@ const awards = [
     description:
       "Awarded Second Place Internationally in the NASA Space Society Settlement Contest.",
     year: "2019",
-  }/*,
+  },
+  {
+  icon: <Star size={20} />,
+  title: "Visa UPLIFT Award – 'Lead Courageously'",
+  description: (
+    <>
+      Recognized by Rahul Mittal, Director at Visa, for exceptional initiative and impact.
+      <br />
+      *“Samyuctaa’s initiative and teamwork are truly appreciated.”*
+    </>
+  ),
+  year: "2025",
+}/*
   {
     icon: <Award size={20} />,
     title: "Visa 'Must Hire' Intern",
