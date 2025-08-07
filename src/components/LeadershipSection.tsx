4762d01efedeb4c6928bd8543f9811afbf149dc0
sriramragav <srirag@gmail.com>
2025-08-07 13:18:17 +0530

    Update LeadershipSection.tsx

--- src/components/LeadershipSection.tsx
+++ src/components/LeadershipSection.tsx
@@ -68,7 +68,7 @@ const LeadershipSection = () => {
           {/* Sam image */}
           <div className="flex justify-center w-full md:w-auto">
             <img
-              src="/lovable-uploads/6d3685c0-e65a-4cd2-a633-6610881b144f.png"
+              src="/avatar/Leadership.png"
               alt="Sam showing project blueprints"
               className="h-28 w-auto"
               loading="lazy"
